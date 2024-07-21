e46830ec85ad27f362f5ffca6d77915d1dc78ca5
Arthur <arthurcalazans@gmail.com>
2024-07-20 21:26:06 -0300

    Funcao posicao dos botoes

--- src/main/java/com/trabf/melodicgusts/Controllers/NewGame/NewGameMatchController.java
+++ src/main/java/com/trabf/melodicgusts/Controllers/NewGame/NewGameMatchController.java
@@ -10,6 +10,7 @@ import javafx.scene.media.MediaView;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -17,15 +18,14 @@ public class NewGameMatchController implements Initializable {
     public Button[][] buttons;
     public GridPane gridPane;
     public Button confirm_btn;
-    public List<Button> buttons_active = new ArrayList<>();
+    public List<Button> active_buttons = new ArrayList<>();
     public Label accept_lbl;
     public Label error_lbl;
     public MediaView m00_media;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        buttons = new Button[gridPane.getRowCount()][gridPane.getColumnCount()];
-
+        this.buttons = new Button[gridPane.getRowCount()][gridPane.getColumnCount()];
         createButtons();
         allButtons();
         confirm_btn.setOnAction(event -> comparisonPiece());  //faz a comparacao
@@ -36,7 +36,6 @@ public class NewGameMatchController implements Initializable {
             for (int j = 0; j < gridPane.getColumnCount(); j++) {
                 buttons[i][j] = new Button();
                 gridPane.add(buttons[i][j], j, i);
-                buttons[i][j].setId("b" + i + j + "_btn");
                 buttons[i][j].setMinWidth(110.4);
                 buttons[i][j].setMinHeight(86.4);
                 buttons[i][j].setStyle("-fx-background-color: #bfb8b8");
@@ -65,7 +64,7 @@ public class NewGameMatchController implements Initializable {
 
     //verifica se existe mais de um botao ativo e para sua musica pq nao pode ter mais de uma musica tocando ao mesmo tempo
     public void openPiece(int row, int col) {
-        if(buttons_active.size() == 2) {
+        if(active_buttons.size() == 2) {
             stopMusic();
         }
         onMusic(row, col);
@@ -82,40 +81,40 @@ public class NewGameMatchController implements Initializable {
 
     public void addButton(Button button) {
         //adiciona o botao na lista de botoes ativos
-        buttons_active.add(button);
+        active_buttons.add(button);
         // verifica se existe mais de um par de botoes na lista, só pode ter 2 boteos na lista na comparacao
-        if(buttons_active.size() == 3) { // se tiver 3 botoes no buffer
+        if(active_buttons.size() == 3) { // se tiver 3 botoes no buffer
             removeButton(button);
         }
     }
 
     public void removeButton(Button button) {
         //habilita o botao que foi removido para poder ser novamente clicado
-        buttons_active.get(1).setDisable(false);
+        active_buttons.get(1).setDisable(false);
         //remove o segundo botao da lista
-        buttons_active.remove(1);
+        active_buttons.remove(1);
         // move o botao novo que estava no index 2 para a posicao do antigo index 1
-        buttons_active.add(1, button);
+        active_buttons.add(1, button);
         // remove o botao que tinha adicionado na lista na funcao adicionarButton() para nao ficar repetido na lista
-        buttons_active.remove(2);
+        active_buttons.remove(2);
     }
 
 
     public void comparisonPiece() {
-        if(buttons_active.size() == 1) {
+        if(active_buttons.size() == 1) {
             stopMusic();
             error_lbl.setText("Error: Não é possivel fazer a comparação com 1 peça");
             error_lbl.setStyle("-fx-text-fill: red; -fx-font-size: 1.3em; -fx-font-weight: bold");
             accept_lbl.setText("");
-        } else if (buttons_active.isEmpty()) {
+        } else if (active_buttons.isEmpty()) {
             error_lbl.setText("Error: Não é possivel fazer a comparação com nenhuma peça");
             error_lbl.setStyle("-fx-text-fill: red; -fx-font-size: 1.3em; -fx-font-weight: bold");
             accept_lbl.setText("");
         } else {
             stopMusic();
 
-            int piece1_id = capturaId(0);
-            int piece2_id = capturaId(1);
+            int piece1_id = captureId(0);
+            int piece2_id = captureId(1);
 
             if (piece1_id == piece2_id) {
                 onAcert();
@@ -123,37 +122,32 @@ public class NewGameMatchController implements Initializable {
                 onError();
             }
             // limpa a lista de botes ativos para poder escolher outras peças
-            buttons_active.clear();
+            active_buttons.clear();
         }
     }
 
-    public int capturaId(int index) {
-        //pegar a string do nome (id do botao) para saber qual a posicao dele na matriz
-        char second_char = buttons_active.get(index).getId().charAt(1);
-        char third_char = buttons_active.get(index).getId().charAt(2);
-
-        //converte char para string e depois string para int
-        int row = Integer.parseInt(String.valueOf(second_char));
-        int col = Integer.parseInt(String.valueOf(third_char));
-
-        // retorna o id da peca - todas pecas possuem um id - duas pecas possuem o mesmo id
+    private int captureId(int index) {
+        Button button = active_buttons.get(index);
+        int row = rowButton(button);
+        int col = colButton(button);
+        // retorna piece Id para comparacao
         return Model.getInstance().getBoard().getPieces()[row][col].getIdPiece();
     }
 
     public void onAcert() {
         // desabilita os botoes pq ja foram acertados
-        buttons_active.get(0).setDisable(true);
-        buttons_active.get(1).setDisable(true);
-        buttons_active.get(0).setText("X");
-        buttons_active.get(1).setText("X");
+        active_buttons.get(0).setDisable(true);
+        active_buttons.get(1).setDisable(true);
+        active_buttons.get(0).setText("X");
+        active_buttons.get(1).setText("X");
         accept_lbl.setText("Acertou !!!");
         accept_lbl.setStyle("-fx-text-fill: green; -fx-font-size: 1.3em; -fx-font-weight: bold");
     }
 
     public void onError() {
         // habilita os botoes para poderem ser clicados
-        buttons_active.get(0).setDisable(false);
-        buttons_active.get(1).setDisable(false);
+        active_buttons.get(0).setDisable(false);
+        active_buttons.get(1).setDisable(false);
         accept_lbl.setText("Errou !!!");
         accept_lbl.setStyle("-fx-text-fill: red; -fx-font-size: 1.3em; -fx-font-weight: bold");
     }
@@ -161,5 +155,20 @@ public class NewGameMatchController implements Initializable {
     public void stopMusic() {
         Model.getInstance().getBoard().getMusic().getMediaPlayer().stop();
     }
-}
 
+    // pegar a posicao do button
+    private int rowButton(Button button) {
+        int row;
+        Iterator<Object> list = button.getProperties().values().iterator();
+        row = (int) list.next();
+        row = (int) list.next();
+        return row;
+    }
+
+    private int colButton(Button button) {
+        int col;
+        Iterator<Object> list = button.getProperties().values().iterator();
+        col = (int) list.next();
+        return col;
+    }
+}
