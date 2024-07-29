8ff2814ef76ffbc206fcb6d1124e4ee5bd44c86c
Arthur <arthurcalazans@gmail.com>
2024-07-29 00:04:55 -0300

    delete confirm_btn

--- src/main/java/com/trabf/melodicgusts/Controllers/NewGame/NewGameCharacterController.java
+++ src/main/java/com/trabf/melodicgusts/Controllers/NewGame/NewGameCharacterController.java
@@ -2,7 +2,6 @@ package com.trabf.melodicgusts.Controllers.NewGame;
 
 import com.trabf.melodicgusts.Models.Model;
 import com.trabf.melodicgusts.Views.UserMenuOptions;
-import javafx.beans.property.StringProperty;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -13,7 +12,6 @@ import java.util.ResourceBundle;
 public class NewGameCharacterController implements Initializable {
     public Button ready_btn;
     public TextField name_fld;
-    public Button confirm_btn;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -21,18 +19,14 @@ public class NewGameCharacterController implements Initializable {
     }
 
     private void addListeners() {
-        confirm_btn.setOnAction(e -> nameUser());
         ready_btn.setOnAction(event -> onReady());
     }
 
     private void onReady() {
-        name_fld.clear();
-        Model.getInstance().getViewFactory().getUserMenuOptions().set(UserMenuOptions.NEWGAME_BOARD);
-    }
-
-    //define o nome do usuario
-    private void nameUser() {
+        //define o nome do usuario
         String name = name_fld.getText();
         Model.getInstance().getUser().nameProperty().set(name);
+        Model.getInstance().getViewFactory().getUserMenuOptions().set(UserMenuOptions.NEWGAME_BOARD);
+        name_fld.clear();
     }
 }
